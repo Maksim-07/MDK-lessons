872af8dd0ad28ad6d1c4f75bb9124e844dc02f1c
Максим <kosmakovmaksim@yandex.ru>
2023-04-07 17:20:47 +0300

    lesson_4 Task_1

---
+++ lesson_4/Task_1.py
@@ -0,0 +1,19 @@
+'''
+Создайте функцию-генератор countdown, которая выводит числа от 10 до 0.
+а) Опустошите генератор с помощью цикла
+б) Опустошите генератор с помощью next()
+'''
+
+
+def countdown():
+    for m in range(10, -1, -1):
+        yield m
+
+
+y = countdown()
+activity = True
+try:
+    while activity:
+        print(next(y))
+except StopIteration as ex:
+    activity = False
